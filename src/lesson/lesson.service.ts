27ca289aff3adf3da124ff02207a7397350f15ee
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-05-07 11:19:28 -0300

    feat: first commit

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson)
+    private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+
+  async getAllLesson(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+}
