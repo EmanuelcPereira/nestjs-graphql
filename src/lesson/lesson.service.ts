f379aaa2043a135932016b43b17ea52ddd068513
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-05-08 16:08:36 -0300

    feat: finish project

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -10,15 +10,16 @@ export class LessonService {
   constructor(
     @InjectRepository(Lesson)
     private lessonRepository: Repository<Lesson>,
-  ) {}
+  ) { }
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
-    const { name, startDate, endDate } = createLessonInput;
+    const { name, startDate, endDate, students } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
       startDate,
       endDate,
+      students,
     });
 
     return this.lessonRepository.save(lesson);
@@ -30,5 +31,14 @@ export class LessonService {
 
   async getAllLesson(): Promise<Lesson[]> {
     return this.lessonRepository.find();
+  }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentIds];
+    return this.lessonRepository.save(lesson);
   }
 }
